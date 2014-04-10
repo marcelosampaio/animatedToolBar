c4dd43a87e9e9da9f086799980debd2a3d973263
Marcelo Sampaio <marcelo@artlogica.com.br>
2014-04-10 10:06:51 -0300

    Initial Commit

---
+++ animatedToolBar/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  animatedToolBar
+//
+//  Created by Marcelo Sampaio on 4/10/14.
+//  Copyright (c) 2014 Marcelo Sampaio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
