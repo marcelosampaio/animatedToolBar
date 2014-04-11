c4c69198ee102b1092f6c097ee716119b812fd66
Marcelo Sampaio <marcelo@artlogica.com.br>
2014-04-11 10:36:39 -0300

    iPad images

--- animatedToolBar/ViewController.m
+++ animatedToolBar/ViewController.m
@@ -11,25 +11,30 @@
 // iPhone 3.5'' inch
 #define DEVICE_IPHONE_35_PORTRAIT_WIDTH             320
 #define DEVICE_IPHONE_35_PORTRAIT_HEIGHT            480
+#define DEVICE_IPHONE_35_PORTRAIT_TOOLBAR_IMAGE     @""
 
 #define DEVICE_IPHONE_35_LANDSCAPE_WIDTH            480
 #define DEVICE_IPHONE_35_LANDSCAPE_HEIGHT           320
-
+#define DEVICE_IPHONE_35_LANDSCAPE_TOOLBAR_IMAGE    @""
 
 // iPhone 4'' inch
 #define DEVICE_IPHONE_40_PORTRAIT_WIDTH             320
 #define DEVICE_IPHONE_40_PORTRAIT_HEIGHT            568
+#define DEVICE_IPHONE_40_PORTRAIT_TOOLBAR_IMAGE     @""
 
 #define DEVICE_IPHONE_40_LANDSCAPE_WIDTH            568
 #define DEVICE_IPHONE_40_LANDSCAPE_HEIGHT           320
+#define DEVICE_IPHONE_35_LANDSCAPE_TOOLBAR_IMAGE    @""
 
 
 // iPad
 #define DEVICE_IPAD_PORTRAIT_WIDTH                  768
 #define DEVICE_IPAD_PORTRAIT_HEIGHT                 1024
+#define DEVICE_IPAD_PORTRAIT_TOOLBAR_IMAGE          @"iPadPortraitToolBar"
 
 #define DEVICE_IPAD_LANDSCAPE_WIDTH                 1024
 #define DEVICE_IPAD_LANDSCAPE_HEIGHT                768
+#define DEVICE_IPAD_LANDSCAPE_TOOLBAR_IMAGE         @"iPadLandscapeToolBar"
 
 
 @interface ViewController ()
@@ -78,7 +83,7 @@
     } else if ([[UIDevice currentDevice] orientation] == UIDeviceOrientationLandscapeRight) {
         orientation=@"Landscape";
     }else{
-        orientation=@"Portrait";
+        return;
     }
     [self setDeviceLogicalSizeForOrientation:orientation];
 }
@@ -91,7 +96,8 @@
 #pragma mark - Working Methods
 -(void)setDeviceLogicalSizeForOrientation:(NSString *)orientation
 {
-    NSLog(@"setDeviceLogicalSize Orientatio=%@",orientation);
+    NSString *toolBarImage=@"";
+    
     BOOL iPhoneDevice=[self deviceRecognition];
     if (iPhoneDevice) {
         // 4 inch  or 3.5 inch screen size (TO DO)
@@ -126,17 +132,19 @@
         if ([orientation isEqualToString:@"Portrait"]) {
             self.deviceWidth=DEVICE_IPAD_PORTRAIT_WIDTH;
             self.deviceHeigth=DEVICE_IPAD_PORTRAIT_HEIGHT;
+            toolBarImage=DEVICE_IPAD_PORTRAIT_TOOLBAR_IMAGE;
         } else {
             self.deviceWidth=DEVICE_IPAD_LANDSCAPE_WIDTH;
             self.deviceHeigth=DEVICE_IPAD_LANDSCAPE_HEIGHT;
+            toolBarImage=DEVICE_IPAD_LANDSCAPE_TOOLBAR_IMAGE;
         }
     }
 
     // Remove views from super view
     [self removeAllSubviewsFromView];
     
-    // Debug purposes
-    [self dropTestImage];
+    // Show Tool Bar
+    [self showToolBarWithImage:toolBarImage];
     
 }
 
@@ -155,7 +163,9 @@
 -(void)removeAllSubviewsFromView
 {
     for (UIView *subview in self.view.subviews) {
-        [subview removeFromSuperview];
+        if (subview.tag==9999) {
+            [subview removeFromSuperview];
+        }
     }
 }
 
@@ -168,26 +178,36 @@
 }
 
 #pragma mark - Debug Methods
--(void)dropTestImage
+-(void)showToolBarWithImage:(NSString *)toolBarImageName
 {
-    NSLog(@"width=%f  height=%f",self.deviceWidth,self.deviceHeigth);
+    UIImageView *toolBarImage=[[UIImageView alloc]initWithFrame:CGRectMake(0, -100, self.deviceWidth, 90)];
+    toolBarImage.image=[UIImage imageNamed:toolBarImageName];
+    toolBarImage.tag=9999;
     
-    UIImageView *testImageView=[[UIImageView alloc]initWithFrame:CGRectMake((self.deviceWidth/2)-25, -100, 50, 50)];
-    testImageView.backgroundColor=[UIColor redColor];
-    
-    [self.view addSubview:testImageView];
+    [self.view addSubview:toolBarImage];
     
     // animate to the bottom of the screen
-    [UIView animateWithDuration:0.80f animations:^(void)
+    [UIView animateWithDuration:0.50f animations:^(void)
      // Aqui se faz a animacao
      {
-         CGPoint position=CGPointMake(testImageView.center.x,self.deviceHeigth);
-         testImageView.center=position;
+         CGPoint position=CGPointMake(toolBarImage.center.x,45);
+         toolBarImage.center=position;
          
      } completion:^(BOOL finished)
      //  Aqui executamos os procedimentos logo apos o termino da animacao
      {
          // Faz nada por enquanto no completion
+         [UIView animateWithDuration:0.95f animations:^(void)
+          // Aqui se faz a animacao
+          {
+              CGPoint position=CGPointMake(toolBarImage.center.x,-100);
+              toolBarImage.center=position;
+              
+          } completion:^(BOOL finished)
+          //  Aqui executamos os procedimentos logo apos o termino da animacao
+          {
+              // Faz nada por enquanto no completion
+          }];
      }];
     // FIM DA ANIMACAO
     [UIView commitAnimations];
